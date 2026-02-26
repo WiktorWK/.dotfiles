452f337ea5593a9f81db3e9e022fc012577101a0
wiktor <wiktorkulicki@gmail.com>
2026-02-26 12:28:48 +0100

    add bash comments

--- brave.sh
+++ brave.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+set -e
+
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 
 sudo curl -fsSLo /etc/apt/sources.list.d/brave-browser-release.sources https://brave-browser-apt-release.s3.brave.com/brave-browser.sources
--- font_install.sh
+++ font_install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 # installs hack nerd font and patches it
 
 sudo apt-get -y install fontforge
