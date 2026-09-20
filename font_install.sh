48968559e6fb5ebc79d9f718d127af7eeca93556
wiktor <wiktorkulicki@gmail.com>
2026-09-20 17:16:00 +0200

    fix nerd fonts

--- font_install.sh
+++ font_install.sh
@@ -3,66 +3,28 @@
 set -euo pipefail
 
 FONT_DIR="$HOME/.local/share/fonts"
-NERD_FONTS_DIR="/tmp/nerd-fonts"
 
 echo
-echo "=== Cleaning previous generated Hack fonts ==="
+echo "=== Cleaning previous Hack fonts ==="
 echo
 
 rm -f \
     "$FONT_DIR"/Hack*.ttf \
     "$FONT_DIR"/Hack*.otf
 
-rm -rf "$NERD_FONTS_DIR"
-
 echo
-echo "=== Clone Nerd Fonts ==="
+echo "=== Installing Hack Nerd Font ==="
 echo
 
-git clone \
-    --depth 1 \
-    https://github.com/ryanoasis/nerd-fonts.git \
-    "$NERD_FONTS_DIR"
-
-echo
-echo "=== Prepare Codicons ==="
-echo
-
-cp \
-    "$NERD_FONTS_DIR/src/glyphs/codicons/codicon.ttf" \
-    "$NERD_FONTS_DIR/src/glyphs/"
-
-echo
-echo "=== Patch Hack ==="
-echo
-
-cd "$NERD_FONTS_DIR"
-
-fontforge \
-    -script "$NERD_FONTS_DIR/font-patcher" \
-    "$NERD_FONTS_DIR/src/unpatched-fonts/Hack/Regular/Hack-Regular.ttf" \
-    -s \
-    -c
-
-echo
-echo "=== Install fonts ==="
-echo
-
-mkdir -p "$FONT_DIR"
-
-cp Hack*.ttf "$FONT_DIR/"
+curl -fsSL \
+    https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/install.sh \
+    | bash -s -- install Hack
 
 echo
 echo "=== Update font cache ==="
 echo
 
 fc-cache -f "$FONT_DIR"
-
-echo
-echo "=== Cleanup ==="
-echo
-
-rm -rf "$NERD_FONTS_DIR"
 
 echo
 echo "=== Installed Hack fonts ==="
